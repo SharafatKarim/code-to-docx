6360eb648667e7f96cd1f1e152942920ea264197
SharafatKarim <sharafat2004@gmail.com>
2023-09-05 21:05:03 +0600

    code addapted to file extension

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from docx.shared import Inches
 import os
 from natsort import natsorted
 
+file_extesion= input("Enter file extension: ")
+
 def write():
     document = Document()
 
@@ -33,18 +35,29 @@ def write():
     files_list = natsorted(files_list)
 
     for i in files_list:
-        if i[-2:]=='.c':
+        # if i[-2:]=='.c':
+        if i.split('.')[-1]==file_extesion:
             print("Found file -> ", i)
 
     for i in range(len(files_list)):
-        if files_list[i][-2:]=='.c':
+        # if files_list[i][-2:]=='.c':
+        if files_list[i].split('.')[-1]==file_extesion:
             file_content = open(files_list[i])
-            document.add_heading(files_list[i][6:-2], level=1)
+            # document.add_heading(files_list[i][6:-2], level=1)
+            document.add_heading(files_list[i].split('.')[0], level=1)
             document.add_paragraph(file_content.read())
             document.add_page_break() # page break
             # document.add_page_break() # page break for screenshots
 
-    document.save('output.docx')
+    if os.path.isfile('output.docx'):
+        print("File exists. Do you want to overwrite it?")
+        user_input = input("Enter y or n: ")
+        if user_input == 'y':
+            document.save('output.docx')
+        else:
+            print("File not saved")
+    else:
+        document.save('output.docx')
 
 if __name__ == "__main__":
     write()
